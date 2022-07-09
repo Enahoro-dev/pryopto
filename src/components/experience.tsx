e4c37764eecad0eea98d334a2daf75330d42c8c5
Abhulimen Enahoro <abhulimenenahoro@gmail.com>
2022-07-09 23:35:03 +0100

    margins fore experience page

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -3,8 +3,8 @@ import {companies} from './companies'
 
 const Experience:React.FC = () => {
   return (
-    <div id='experience' className='flex text-center bg-lightgrey w-full flex-col pb-16'>
-        <h2 className='text-newblack text-3xl font-bold mt-8 mb-2 lg:text-4xl'>Trust Our Experience</h2>
+    <div id='experience' className='flex text-center bg-lightgrey w-full flex-col pb-12'>
+        <h2 className='text-newblack text-3xl font-bold mt-12 mb-2 lg:text-4xl'>Trust Our Experience</h2>
         <p className='w-3/5 mx-auto text-sm mb-4'>We have worked with top brands, and we have the reach you need.</p>
         <div className='flex flex-wrap justify-center items-center w-3/5 mx-auto'>
             {companies.map((company)=>(
