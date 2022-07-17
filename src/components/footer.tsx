ad3d61af8ba0168a318f9a6b2a2b84e77571bdd0
Abhulimen Enahoro <abhulimenenahoro@gmail.com>
2022-07-17 19:07:46 +0100

    added sm links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,8 +6,8 @@ const Footer:React.FC = () => {
   return (
     <div className=' bg-newblack pt-16 text-center text-newwhite pb-20'>
         <div className="flex justify-center items-center mb-4">
-            <FaInstagram className='text-3xl mr-4'/>
-            <FaTwitter className='text-3xl'/>
+            <a className='text-newwhite' href='https://www.instagram.com'><FaInstagram className='text-3xl mr-4'/></a>
+            <a className='text-newwhite' href='https://www.twitter.com/pryopto'><FaTwitter className='text-3xl'/></a>
         </div>
         <p className="mb-4">Email: info@pryopto.com</p>
         <p className="mb-4">Telephone: +234 706 722 0619</p>
